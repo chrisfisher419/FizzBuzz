8cdfe3cafa5ce09ce1e1b8cadae001603e105c04
Christopher Fisher <chrisfisher419@gmail.com>
2018-11-18 17:53:20 -0500

    Update FizzBuzz.cs

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -23,7 +23,7 @@ namespace FizzBuzz
                 }
                 else if (i % 5 == 0)
                 {
-                    Console.WriteLine("fizz");
+                    Console.WriteLine("buzz");
                 }
                 else
                 {
